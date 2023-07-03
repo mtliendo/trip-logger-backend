aea95c0146b7f76316a3c86d8a1b69d0076c9f63
focusotter <mtliendo@focusotter.com>
2023-07-03 13:41:03 -0500

    Initial commit

---
+++ test/trip-logger-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TripLoggerBackend from '../lib/trip-logger-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trip-logger-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TripLoggerBackend.TripLoggerBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
